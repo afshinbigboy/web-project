5f8cf5623c6eec6f993bea37ec081f60d34ab763
afshinbigboy <afshinbigboy@hotmail.com>
2013-12-26 01:09:09 +0330

    marketing web project

--- product/urls.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'Afshin'
-
-from django.conf.urls import patterns,include, url
-
-urlpatterns = patterns('product.views',
-    url(r'^list/', 'product_list', name='product_list'),
-    url(r'^item/', 'product_details', name='product_details'),
-)
